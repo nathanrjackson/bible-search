941e2bec2d469e3105808522ce9b566a676331c6
nathanrjackson <nathanrjackson30@gmail.com>
2024-12-05 17:47:09 -0700

    Removed tkinter elements and started again from scratch. Basic outline and functionality to find scriptures added

--- bible_search.py
+++ bible_search.py
@@ -1,115 +1,78 @@
-import tkinter as tk
-from tkinter import ttk
 import csv
+import pytest
+import random
+
+# VERSE_ID_INDEX = 0
+BOOK_NAME_INDEX = 1
+BOOK_NUMBER_INDEX = 2
+CHAPTER_INDEX = 3
+VERSE_INDEX = 4
+TEXT_INDEX = 5
 
 
 def main():
-    # Create the main window
-    root = tk.Tk()
-    root.title("Bible Search")
-    root.geometry("800x600")
+    # Create a compound list of each verse in the Bible
+    kjv_compound_list = read_csv_file("kjv.csv")
 
-    # Add a welcome message and short description
-    welcome = tk.Label(root, text="Welcome to Bible Search!", font=("Arial", 16))
-    desc = tk.Label(root, text="Enter keyword or phrase below", font=("Arial", 16))
-    welcome.pack()
-    desc.pack()
+    # Get keyword(s) from user
+    keywords = input("Enter a word or phrase to search: ")
 
-    # Add an entry field for name
-    entry = tk.Entry(root)
-    entry.pack()
+    # Search the Bible for keyword(s)
+    scriptures = find_scriptures(kjv_compound_list, keywords)
 
-
-    # Function to update the UI
-    def update_label(entry, submit_button):
-        # Get the text from the entry before destroying the widgets
-        user_input = entry.get()
-        
-        # Destroy the button
-        submit_button.destroy()
-
-        # Clear all widgets
-        scriptures = find_scripture(user_input)
-        
-        if len(scriptures) == 1:
-            presentation_text = "Here is your scripture:"
-        elif len(scriptures) > 1:
-            presentation_text = "Here are your scriptures:"
-        else:
-            presentation_text = "No scriptures found"
-
-        presentation_text = tk.Label(root, text=presentation_text, font=("Arial", 14), wraplength=600)
-        presentation_text.pack()
-        
-        for scripture in scriptures:
-            # Add a new label with personalized text
-            new_label = tk.Label(root, text=scripture, font=("Arial", 14), wraplength=600)
-            new_label.pack()
-        
-        # Add a "Search Again" button
-        search_again_button = tk.Button(root, text="Search Again", command=reset_ui)
-        search_again_button.pack()
-
-
-    # Function to reset the UI
-    def reset_ui():
-        # Clear all widgets
-        for widget in root.winfo_children():
-            widget.destroy()
-        
-        # Recreate the original UI
-        # global welcome, desc, entry
-        welcome = tk.Label(root, text="Welcome to Bible Search!", font=("Arial", 16))
-        desc = tk.Label(root, text="Enter keyword or phrase below", font=("Arial", 16))
-        welcome.pack()
-        desc.pack()
-
-        new_entry = tk.Entry(root)
-        new_entry.pack()
-
-        button = tk.Button(root, text="Submit", command=lambda: update_label(new_entry, button))
-        button.pack()
-
-
-    # Add a button
-    button = tk.Button(root, text="Submit", command=lambda: update_label(entry, button))
-    button.pack()
-
-    # Run the application
-    root.mainloop()
-
-
-def find_scripture(user_input):
-    VERSE_ID_INDEX = 0
-    BOOK_NAME_INDEX = 1
-    BOOK_NUMBER_INDEX = 2
-    CHAPTER_INDEX = 3
-    VERSE_INDEX = 4
-    TEXT_INDEX = 5
-    
-    requested_scriptures = []
-
-    scriptures = read_csv_file("kjv.csv")
-
-    for scripture in scriptures:
-        if user_input.lower() in scripture[TEXT_INDEX].lower():
-            requested_scriptures.append(scripture[TEXT_INDEX])
-    
-    return requested_scriptures
+    print(scriptures)
 
 
 def read_csv_file(filename):
-    scriptures = []
-    
+    """Read a CSV file, skip the first line, and append 
+    each line to a list
+    Parameter: csv filename
+    Return: a compound list
+    """
+    # Create an empty list
+    compound_list = []
+
+    # Read CSV file
     with open(filename, "rt") as csv_file:
         reader = csv.reader(csv_file)
 
+        # Skip the first line (header)
         next(reader)
 
         for line in reader:
-            scriptures.append(line)
+            compound_list.append(line)
 
-        return scriptures
+    # Return compound list
+    return compound_list
+
+
+def find_scriptures(kjv_compound_list, keywords):
+    """Search for keyword(s) within a compound list
+    Parameters:
+        compound list
+        keyword(s)
+    Return: a list of scriptures that 
+    include keyword(s)
+    """
+    # Create an empty list
+    scriptures = []
+
+    # Search each verse for keywords
+    for verse in kjv_compound_list:
+        verse_text = verse[TEXT_INDEX]
+
+        if keywords.lower() in verse_text.lower():
+            scriptures.append(verse)
+
+    return scriptures
+
+
+def navigate_scriptures():
+    pass
+
+
+def random_scripture():
+    pass
 
 
 if __name__ == "__main__":
